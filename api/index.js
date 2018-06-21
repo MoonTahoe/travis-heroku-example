f753b2e781a0ed327d0b02c5603ee953c0e7004d
MoonTahoe <alex@moonhighway.com>
2018-06-21 11:42:35 -0700

    Use of const not allowed for some reason

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const path = require('path')
+var express = require('express')
+var path = require('path')
 
 express()
     .use(express.static(path.join(__dirname, '..', 'build')))
