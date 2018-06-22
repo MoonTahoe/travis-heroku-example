acb3f1c2b33d98ffe7c35949c530a328fbc5e9e2
MoonTahoe <alex@moonhighway.com>
2018-06-22 15:44:04 -0700

    Changing hosting to Serverless Netlify

--- api/index.js
+++
@@ -1,6 +0,0 @@
-var express = require('express')
-var path = require('path')
-
-express()
-    .use(express.static(path.join(__dirname, '..', 'build')))
-    .listen(process.env.PORT || 3000, () => console.log('application running on 3000'))
